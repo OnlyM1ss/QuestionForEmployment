b4422db2590f11f2b9d67d17e974470cb71fdc8e
Nick <sojiguy@mail.ru>
2020-12-02 03:20:57 +0400

    added first quest

---
+++ TestTaskIRZ/MemoryDataSource.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestTaskIRZ
+{
+    public class MemoryDataSource
+    {
+        private List<User> Users = new List<User>();
+        private List<string> userNames = new List<string>()
+        {
+            "Rachel Thompson",
+            "Anthony Roberts",
+            "Lisa Ruiz",
+            "Raymond Chandler",
+            "Lauren Rodriguez",
+            "Bernice Morris",
+            "Martha Evans",
+            "Joel Kim",
+            "Nikita_2010",
+            "Bernice Morris",
+            "Diane Edwards"
+        };
+        public void SetUser(User user)
+        {
+            Users.Add(user);
+        }
+        public User GetUser(int ind)
+        {
+            return Users[ind];
+        }
+        public List<User> GetAllUsers()
+        {
+            List<User> users = new List<User>();
+            users = Users;
+            return users;
+        }
+        public string GetNick(int ind)
+        {
+            return userNames[ind];
+        }
+
+    }
+}
---
+++ TestTaskIRZ/Server.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestTaskIRZ
+{
+    public class Server
+    {
+        private string connectIsSuccessfull()
+        {
+            return "Hi,great job, you have been find correct Port,\n but it's not end,you need find correct user for continue";
+        }
+        private string Help()
+        {
+            return "you need make code review for find correct user";
+        }
+        /// <summary>
+        /// generate random port for connect
+        /// </summary>
+        /// <returns>random port</returns>
+        private int GeneratePort()
+        {
+            Random random = new Random();
+            return random.Next(4000, 7000);
+        }
+        public void RunServer()
+        {
+            IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), GeneratePort());
+                while (true)
+                {
+                using (Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+                {
+                    socket.Bind(ipPoint);
+                    socket.Listen(10);
+                    using Socket handler = socket.Accept();
+                    StringBuilder builder = new StringBuilder();
+                    int bytes = 0;
+                    byte[] data = new byte[256];
+                    do
+                    {
+                        try
+                        {
+                            bytes = handler.Receive(data);
+                            builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                        }
+                        catch
+                        {
+                            continue;
+                        }
+                    }
+                    while (handler.Available > 0);
+                    Console.WriteLine(DateTime.Now.ToShortTimeString() + ": " + builder.ToString());
+                    byte[] WelcomeData = new byte[256];
+                    WelcomeData = Encoding.Unicode.GetBytes(connectIsSuccessfull() + "\n" + Help());
+                    handler.Send(WelcomeData);
+                }
+                }
+        }
+    }
+}
